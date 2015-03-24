2bd2f5b4c7917c9c479c0a183e1ac92e7b616613
Naijia Liu <njliu@thoughtworks.com>
2015-03-24 17:00:09 +0800

    init

---
+++ fugue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fugue
+//
+//  Created by njliu on 3/24/15.
+//  Copyright (c) 2015 Naijia Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
