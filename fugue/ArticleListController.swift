83cb07f0a17eb04aa773521888e717c2f05ec33c
Naijia Liu <njliu@thoughtworks.com>
2015-04-02 15:04:29 +0800

    use UITableViewController instead of UIViewController

--- fugue/ArticleListController.swift
+++ fugue/ArticleListController.swift
@@ -8,38 +8,52 @@
 
 import UIKit
 
-class ArticleListController: UIViewController {
+class ArticleListController: UITableViewController, UITableViewDataSource {
 
-    @IBOutlet weak var articleList: ArticleListView!
+    //@IBOutlet weak var articleList: ArticleListView!
+    var articles: [Article]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // auto-resizing row height
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 80
+
         
-        loadSavedArticles()
+        let repo = ArticleRepo()
+        self.articles = repo.getAll()
         
-        let url = NSURL(string: "http://www.infoq.com/cn/news")
+        //loadSavedArticles()
+        
+//        let url = NSURL(string: "http://www.infoq.com/cn/news")
         //downloadArticles(url)
         
         //let url = NSURL(string: "https://avatars3.githubusercontent.com/u/3588994?v=3&s=96")
         //let data = NSData(contentsOfURL: url!, options: nil, error: nil)
-        return
+//        return
         
         
-        let x = NSURL(string: "http://www.infoq.com/cn/news/2015/03/apache-kafka-stream-data-advice")
-        HttpClient.get(x!, parameters: nil, responseSerializer: InfoQArticleResponseSerializer())
-        {
-            (articleBody: ArticleBody) -> Void in
-            //println(articleBody.content)
-        }
+//        let x = NSURL(string: "http://www.infoq.com/cn/news/2015/03/apache-kafka-stream-data-advice")
+//        HttpClient.get(x!, parameters: nil, responseSerializer: InfoQArticleResponseSerializer())
+//        {
+//            (articleBody: ArticleBody) -> Void in
+//            //println(articleBody.content)
+//        }
 
     }
     
-    func loadSavedArticles() {
-        let repo = ArticleRepo()
-        let articles = repo.getAll()
-        articleList.articles = articles
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles != nil ? articles!.count : 0
     }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("articleCell") as ArticleListViewCell
+        cell.set(articles?[indexPath.row])
+        return cell
+    }
+    
 
     func downloadArticles(articleListUrl: NSURL?) {
         HttpClient.get(articleListUrl!, parameters: nil, responseSerializer: InfoQArticleListResponseSerializer()) {
@@ -63,7 +77,10 @@ class ArticleListController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let articleController = segue.destinationViewController as? ArticleController {
-            articleController.article = articleList.selectedArticle
+            if let index = tableView.indexPathForSelectedRow()?.row {
+                let selectedArticle = articles?[index]
+                articleController.article = selectedArticle
+            }
         }
     }
 }
